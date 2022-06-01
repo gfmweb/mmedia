d48d351490a7cc82f47c0646f066961fafb86f6e
gfm <gfmwebpost@gmail.com>
2022-06-01 13:00:49 +0500

    Расчет длины пробега + допы

--- calc/calc.php
+++ calc/calc.php
@@ -12,23 +12,82 @@
 	$config  =  require 'config.php'; // Конфигурационный файл
 	require 'db/CRUD.php'; // PDO БД класс
 	$DB  = new CRUD($config);  // Подключились к БД
+	$Calc = new Calculator();
 	// Раскомментировать когда в БД появятся записи
 	
 	/* $query = 'SELECT * FROM `table` WHERE 1';
-	$DB->command($query,[])->Resulting; */
-	$Calc = new Calculator();
+	$DBPrice =  $DB->command($query,[])->Resulting; */
 	
-	$response = new Response(['result'=>$Calc->calculate(123,10)],200);
+	// Сбор параметров от пользователя
+	$latFrom = $_REQUEST['from_lat']; // Откуда высота
+	$lonFrom = $_REQUEST['from_long']; // Откуда долгота
+	$latTo   = $_REQUEST['to_lat']; // Куда высота
+	$lonTo   = $_REQUEST['to_long']; // Куда долгота
+	$Car     = $_REQUEST['car']; // Какая машина
+	$Mass    = $_REQUEST['mass']; // Вес
+	$Vol     = $_REQUEST['vol']; // объем
+	$Dops = json_decode($_REQUEST['dops'],true); // JSON массив всяких допов
+	// Сбор параметров от пользователя
+	$dops = [];
+	foreach ($Dops as $el){ // Получение информации по допам в соответствии с запросом
+		// Условие наполнения массива допов
+	}
+	
+	
+	$Distanse  = Calculator::Distanse($latFrom,$lonFrom,$latTo,$lonTo); // Количество км пробега
+	
+	$price  = 	Calculator::GetPriceByCar($DBPrice,$Car); // Стоимость 1 км пробега в зависимости от АМ
+	
+	
+	$response = new Response(['result'=>$Calc->calculate($Distanse,$price)],200);
 	
 	//
 	class Calculator
 	{
+		public static function GetPriceByCar($DBprice,$Car)
+		{
+			// Условия поиска нужных значений из БД
+			return [];
+		}
+		
+		
+		public static function Distanse($latitudeFrom, $longitudeFrom, $latitudeTo, $longitudeTo)
+			{
+				  // convert from degrees to radians
+				  $earthRadius = 6371000;
+				  $latFrom = deg2rad($latitudeFrom);
+				  $lonFrom = deg2rad($longitudeFrom);
+				  $latTo = deg2rad($latitudeTo);
+				  $lonTo = deg2rad($longitudeTo);
+				
+				  $lonDelta = $lonTo - $lonFrom;
+				  $a = pow(cos($latTo) * sin($lonDelta), 2) +
+			      pow(cos($latFrom) * sin($latTo) - sin($latFrom) * cos($latTo) * cos($lonDelta), 2);
+				  $b = sin($latFrom) * sin($latTo) + cos($latFrom) * cos($latTo) * cos($lonDelta);
+				
+				  $angle = atan2(sqrt($a), $b);
+				  return (($angle * $earthRadius)/1000)*1.8;
+			}
+		
 		public function calculate( int $distance, int $price, array $dops = null):int
 		{
-			return (is_null($dops)) ? $distance*$price : 0;
+			$nominal = $distance*$price; // Чистая стоимость пробега автомобиля
+			$nominalClear = $distance*$price; // Эталонное значение
+			foreach ($dops as $key) // Бежим по массиву пришедших допов
+			{
+				if($key['mode']=='static'){ // Доп статичный
+					$nominal = $nominal+ $key['summ']; // Прибавляем к цене доп
+				}
+				elseif($key['mode']=='dinamic'){ // Доп процентный
+					$nominal = ($nominalClear*$key['summ'])/100 + $nominal; // Прибавляем процент наценки к эталонной стоимости всего маршрута
+				}
+			}
+			return round( $nominal,0);
 			
 		}
 	}
+	
+	
 	
 	class Response
 	{
